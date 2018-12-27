ebd05dc94a7206109e5819713b6fc8ed51a71a7c
rafaelengc1 <regonzalezc@correo.udistrital.edu.co>
2018-12-27 11:34:12 -0600

    Información relevante de los personajes incluidos en tekken 7, el codigo incluye cambio de estilos en el basemap, consultas sobre la capa y un go to a sitio de origen del personaje

--- js/map.js
+++ js/map.js
@@ -1,14 +1,19 @@
 
 var per = "";
 var variab=0;
-
+var nombre;
+var origen= "";
+var edad= "";
+var enemigos= "";
+var lucha= "";
+var apariciones= "";
+var lon=0;
+var lat=0;
+var video= "";
+         
   
 
-var dojoConfig = {
-  has:{
-    "esri-featurelayer-webgl": 1
-  }
-};
+
 function getRandomInt(max) {
  return Math.floor(Math.random() * Math.floor(max));
 }
@@ -39,65 +44,40 @@ require([
         element.addEventListener('click', function (event) {
           //console.log(event.currentTarget.attributes[1].nodeValue);
           var nombrePersonaje = this.getAttribute("data-description");
+          queryLayer(nombrePersonaje);
 
+          map.removeAll();
+          //map.remove(tileLayerA);
+          //map.remove(tileLayer);
+          //map.remove(featureLayer);
 
-          map.remove(tileLayer);
-          map.remove(featureLayer);
-          map.remove(tileLayerA);
-
+          view.goTo({
+              center: [lon, lat],
+              zoom: 4.5
+          })
+          
           var arreglo = ["https://www.arcgis.com/sharing/rest/content/items/2f00e863b8f446fb8ad1a613d21b56ce/resources/styles/root.json", "https://www.arcgis.com/sharing/rest/content/items/f4292dec0d6546b88cb0b14ed11a27fd/resources/styles/root.json", "https://www.arcgis.com/sharing/rest/content/items/87f5276a633e412287a30c74ea00d3c2/resources/styles/root.json", "http://angpezam.maps.arcgis.com/sharing/rest/content/items/cba6e359a8724790aecea9e0aa28345f/resources/styles/root.json", "http://angpezam.maps.arcgis.com/sharing/rest/content/items/54e25f548fc949ae852ed36b4a65fbb8/resources/styles/root.json"]; 
-            var i = getRandomInt(4);
+          var arreglo2= ["Duomo Di Sirio", "Kinder Gym", "Brimstone & Fire", "Abandoned Temple", "Geometric Plane"]  
+            var i = getRandomInt(5);
             var tileLayerA = new VectorTileLayer({
            // URL to the vector tile service
               url: arreglo[i]
              });
+            var escenario= arreglo2[i]
             map.add(tileLayerA);
             map.add(featureLayer);
 
+
+              document.getElementById("nombre").innerHTML = nombre;
+              document.getElementById("origen").innerHTML = origen;
+              document.getElementById("edad").innerHTML = edad;
+              document.getElementById("enemigos").innerHTML = enemigos;
+              document.getElementById("lucha").innerHTML = lucha;
+              document.getElementById("apariciones").innerHTML = apariciones;  
+              document.getElementById("escenario").innerHTML = escenario;
+              document.getElementById("video").src = video;
         }); 
       });
-       
-         var myFunction= function(){
-          
-        }
-
-       
-       
-      /*document.getElementsByClassName('periodic-element')[1].addEventListener('click', function (event) {
-            // do something
-      
-            //debugger;
-            map.remove(tileLayer);
-      
-
-            var arreglo = ["https://www.arcgis.com/sharing/rest/content/items/2f00e863b8f446fb8ad1a613d21b56ce/resources/styles/root.json", "https://www.arcgis.com/sharing/rest/content/items/f4292dec0d6546b88cb0b14ed11a27fd/resources/styles/root.json", "https://www.arcgis.com/sharing/rest/content/items/87f5276a633e412287a30c74ea00d3c2/resources/styles/root.json", "http://angpezam.maps.arcgis.com/sharing/rest/content/items/cba6e359a8724790aecea9e0aa28345f/resources/styles/root.json", "http://angpezam.maps.arcgis.com/sharing/rest/content/items/54e25f548fc949ae852ed36b4a65fbb8/resources/styles/root.json"]; 
-            var i = getRandomInt(4);
-            var tileLayerA = new VectorTileLayer({
-           // URL to the vector tile service
-              url: arreglo[i]
-             });  
-            map.add(tileLayerA);
-            map.add(featureLayer);
-              per = event.currentTarget.attributes["data-description"].nodeValue;
-              console.log (per);
-            
-
-            if (nombrePersonaje  == "Panda") {
-
-              document.getElementById("nombre").innerHTML = attributes.Personajes;
-              document.getElementById("origen").innerHTML = attributes.Personajes;
-              document.getElementById("edad").innerHTML = attributes.Personajes;
-              document.getElementById("enemigos").innerHTML = attributes.Personajes;
-              document.getElementById("lucha").innerHTML = attributes.Personajes;
-              document.getElementById("apariciones").innerHTML = attributes.Personajes;
-
-            }
-
-
-
-
-  
-        //});*/
 
   var map = new Map();
 
@@ -126,6 +106,30 @@ require([
   });
   view.ui.add(homeButton, "top-right");
 //  ====================================================================================================================
+  function getValues(response) {
+          nombre= response.features[0].attributes.Personaje;
+          origen= response.features[0].attributes.Origen;
+          edad= response.features[0].attributes.Edad;
+          enemigos= response.features[0].attributes.Enemigos;
+          lucha= response.features[0].attributes.Estilo_de;
+          apariciones= response.features[0].attributes.Aparicione;
+          lon= response.features[0].attributes.Longitud;
+          lat= response.features[0].attributes.Latitud;
+          video= response.features[0].attributes.Video;
+        }
+
+  function queryLayer(nombrePersonaje){
+    var query = featureLayer.createQuery();
+query.where = "Personaje = '"+nombrePersonaje+"'";
+query.outFields = [ "*"];
+
+featureLayer.queryFeatures(query)
+  .then(function(response){
+   getValues(response);
+   
+   });
+  }
+        
 
 });
 
